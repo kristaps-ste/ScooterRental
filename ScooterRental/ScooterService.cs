102ecc55f895a8ba873d066688ce02cc692e6e81
kristaps_ste <kristaps.ste@gmail.com>
2020-08-18 11:43:27 +0300

    Scooter service class added

---
+++ ScooterRental/ScooterService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScooterRental
+{
+    class ScooterService : IScooterService
+    {
+        public void AddScooter(string id, decimal pricePerMinute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveScooter(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Scooter> GetScooters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scooter GetScooterById(string scooterId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
